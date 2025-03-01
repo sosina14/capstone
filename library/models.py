5b7161e97bd436e1794a699a1e8086f5541646b2
sosina14 <sosiayu14@gmail.com>
2025-03-01 19:35:49 +0300

    shecode capstone project 2nd step creating a models

--- library/models.py
+++ library/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
 # Create your models here.
+# Create a Book model with attributes: title, author, genre, status
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    genre = models.CharField(max_length=255)
+    status = models.CharField(max_length=255)
+
+def __str__(self):
+        return self.title
